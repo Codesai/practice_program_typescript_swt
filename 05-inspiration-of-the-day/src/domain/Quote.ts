ae76103d284af7c1159e9bbfc02a5b0e05af781e
trikitrok <trikitrok@gmail.com>
2026-01-15 20:48:24 +0100

    adding Qoute author

--- 05-inspiration-of-the-day/src/domain/Quote.ts
+++ 05-inspiration-of-the-day/src/domain/Quote.ts
@@ -1,7 +1,9 @@
 export class Quote {
     readonly text: string;
+    readonly author: string;
 
-    constructor(text: string) {
+    constructor(text: string, author: string) {
         this.text = text;
+        this.author = author;
     }
 }
